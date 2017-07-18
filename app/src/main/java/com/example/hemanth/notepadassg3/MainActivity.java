db839ca87779914d7b1f600734453be1ec7c8545
Hemanth Balakrishna <hbalakrishna@hawk.iit.edu>
2017-07-18 23:51:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hemanth/notepadassg3/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.hemanth.notepadassg3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.note_detail:
+                break;
+            case R.id.about:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
